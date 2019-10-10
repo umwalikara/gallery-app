1263286698af7e62b50e27d56f8fec845c14e1b0
umwalikara <umwaliclara@gmail.com>
2019-10-10 12:44:35 +0200

    first commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wecode/Documents/Core-Friday-Ip/personal-gallery-app/virtual/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
