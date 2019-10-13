6309c04774462f808151b4cb759034c46fbf67ab
umwalikara <umwaliclara@gmail.com>
2019-10-13 19:24:28 +0200

    modify in view

--- gallery/views.py
+++ gallery/views.py
@@ -44,20 +44,3 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
-
-# View Function to present gallery from past days
-def past_days_gallery(request, past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-    except ValueError:
-        Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(gallery_of_day)
-
-    return render(request, 'all-news/past-gallery.html', {"date": date})
