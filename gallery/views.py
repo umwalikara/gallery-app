79e0c268e668a7d790f4f8f1efe930b95b3fcc19
umwalikara <umwaliclara@gmail.com>
2019-10-13 19:17:26 +0200

    adding view file

--- gallery/views.py
+++ gallery/views.py
@@ -17,22 +17,22 @@ def gallery_of_day(request):
     gallery = Image.get_all_images()
     return render(request, 'all-gallery/today-gallery.html', {"date": date,"gallery":gallery})
 
-# def search_results(request):
+def search_results(request):
 
-#     if 'image' in request.GET and request.GET["image"]:
-#         search_term = request.GET.get("image")
-#         searched_images = Image.search_by_category(search_term)
-#         message = f"{search_term}"
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
 
-#         return render(request, 'all-gallery/search.html',{"message":message,"images": searched_images})
+        return render(request, 'all-gallery/search.html',{"message":message,"images": searched_images})
 
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'all-gallery/search.html',{"message":message})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-gallery/search.html',{"message":message})
 
-# def filter_by_lacation (request,location_id):
-#     images = Image.filter_by_location(id=location_id)
-#     return render(request,"all-gallery/location.html", {"images":images})
+def filter_by_lacation (request,location_id):
+    images = Image.filter_by_location(id=location_id)
+    return render(request,"all-gallery/location.html", {"images":images})
 
 def convert_dates(dates):
 
