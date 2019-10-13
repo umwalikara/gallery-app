191fbff3f95f1bc29e64bd558a9ba63167b314da
umwalikara <umwaliclara@gmail.com>
2019-10-13 19:15:30 +0200

    adding admin.html

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,6 +6,6 @@ from .models import Image,Category,Location
 
 # Register your models here.
 
-# admin.site.register(Image)
-# admin.site.register(Category)
-# admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
